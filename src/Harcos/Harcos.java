64f093be29cb0d40dbba99be42eb5d52bc5f1eab
bibor-01 <balogh01biborka@gmail.com>
2022-04-18 16:23:27 +0200

    Harcos osztály túlterhelt metódus, eros és ugyesseg adattag rendezése

--- src/Harcos/Harcos.java
+++ src/Harcos/Harcos.java
@@ -1,17 +1,46 @@
 package Harcos;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+
+class EroComparator implements Comparator<Harcos> {
+
+    @Override
+    public int compare(Harcos egyik, Harcos masik) {
+        return egyik.getEro() - masik.getEro();
+    }
+}
+
+class UgyessegComparator implements Comparator<Harcos> {
+
+    @Override
+    public int compare(Harcos egyik, Harcos masik) {
+        return egyik.getUgyesseg() - masik.getUgyesseg();
+    }
+}
 
 public class Harcos extends Karakter implements Iterable<Eszkoz> {
 
     private int ero, ugyesseg;
 
+    public static EroComparator EroRendezo() {
+        return new EroComparator();
+    }
+
+    public static UgyessegComparator UgyessegRendezo() {
+        return new UgyessegComparator();
+    }
+
     public Harcos(int ero, int ugyesseg, String nev, String faj) {
         super(nev, faj);
         this.ero = ero;
         this.ugyesseg = ugyesseg;
+    }
+
+    public Harcos(int ero, int ugyesseg, String nev) {
+        this(ero, ugyesseg, nev, "ember");
     }
 
     public int getEro() {
